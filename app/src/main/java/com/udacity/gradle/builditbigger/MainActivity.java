f586d15d28ab293959f3be0fad22a77cdf94d09c
Giovani Gonzalez <ggonzalez@itexico.net>
2016-02-21 12:58:05 -0600

    Adding AdMod

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -9,18 +9,41 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.darkgeat.jokesandroidlibrary.JokeActivity;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 import com.udacity.gradle.listeners.OnJokeListener;
 import com.udacity.gradle.network.JokeTask;
 
 
 public class MainActivity extends AppCompatActivity implements OnJokeListener{
 
+    private InterstitialAd interstitialAd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        interstitialAd = new InterstitialAd(this);
+        interstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        interstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                requestNewInterstitial();
+                new JokeTask(MainActivity.this).execute();
+            }
+        });
+        requestNewInterstitial();
     }
 
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        interstitialAd.loadAd(adRequest);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,7 +69,11 @@ public class MainActivity extends AppCompatActivity implements OnJokeListener{
 
     public void tellJoke(View view){
         //Toast.makeText(this, JokesProvider.getJokes(), Toast.LENGTH_SHORT).show();
-        new JokeTask(this).execute();
+        if (interstitialAd.isLoaded()) {
+            interstitialAd.show();
+        }else {
+            new JokeTask(this).execute();
+        }
     }
 
 
