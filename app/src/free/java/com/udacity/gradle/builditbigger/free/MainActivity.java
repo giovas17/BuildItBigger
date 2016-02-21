a27a7df00dacea58593b9b0d572e8eb55c53b14e
Giovani Gonzalez <ggonzalez@itexico.net>
2016-02-21 16:36:23 -0600

    Customizing Flavors

--- app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivity.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivity.java
@@ -1,13 +1,91 @@
 package com.udacity.gradle.builditbigger.free;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.darkgeat.jokesandroidlibrary.JokeActivity;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.udacity.gradle.builditbigger.R;
+import com.udacity.gradle.listeners.OnJokeListener;
+import com.udacity.gradle.network.JokeTask;
+
+public class MainActivity extends AppCompatActivity implements OnJokeListener{
+
+    private InterstitialAd interstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        interstitialAd = new InterstitialAd(this);
+        interstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        interstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                requestNewInterstitial();
+                new JokeTask(MainActivity.this).execute();
+            }
+        });
+        requestNewInterstitial();
+    }
+
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        interstitialAd.loadAd(adRequest);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void tellJoke(View view){
+        //Toast.makeText(this, JokesProvider.getJokes(), Toast.LENGTH_SHORT).show();
+        if (interstitialAd.isLoaded()) {
+            interstitialAd.show();
+        }else {
+            new JokeTask(this).execute();
+        }
+    }
+
+
+    @Override
+    public void OnSuccess(String response) {
+        Intent intent = new Intent(MainActivity.this, JokeActivity.class);
+        intent.putExtra("joke", response);
+        startActivity(intent);
+    }
+
+    @Override
+    public void OnError(String error) {
+        Toast.makeText(this, error, Toast.LENGTH_LONG).show();
     }
 }
